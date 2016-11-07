e93b16579be0a47a3d4f0bdcfbd3115e91b0db75
KieraninTehdas <k.porter.phys@gmail.com>
2016-11-07 15:31:49 +0000

    Add files via upload

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -27,7 +27,10 @@ RunAction::RunAction()
 	analysisManager->CreateNtupleDColumn("PhiInit");
 	analysisManager->FinishNtuple();
 
-	analysisManager->CreateH1("0", "Energy Angular Distribution", 180, 0.0, 50.0*MeV);
+	//analysisManager->CreateH1("0", "Energy Angular Distribution", 180, 0.0, 50.0*MeV);
+	
+	analysisManager->CreateH2("AngularDist", "Energy Loss Theta Dependence", 
+		100.0, 0.0, 1.6*rad, 250.0, 0.0, 50.0*MeV);
 
 }
 
