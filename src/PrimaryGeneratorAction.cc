dbe81482c0d9f03f6d4d52b9d5606b58b56c2766
Kieran Porter <k.porter.phys@gmail.com>
2016-12-15 12:36:03 +0000

    Changed primary action to look at phi dependence

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -65,7 +65,8 @@ void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 
 	G4double cosTheta = -1.0 + 2.0*G4UniformRand();
 	G4double Theta = acos(cosTheta);
-	G4double phi = CLHEP::twopi*G4UniformRand();
+	//G4double phi = CLHEP::twopi*G4UniformRand();
+	G4double phi = 0.53 * G4UniformRand();
 	G4double sinTheta = sqrt(1.0 - cosTheta*cosTheta);
 
 	fParticleGun -> SetParticleMomentumDirection(G4ThreeVector(sinTheta*cos(phi),
